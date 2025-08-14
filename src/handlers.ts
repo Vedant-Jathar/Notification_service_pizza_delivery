8f1451e271c38d5e0332528e55cc0a5f709622a6
Vedant_Jathar <jatharvedant16@gmail.com>
2025-08-14 14:58:52 +0530

    modify the html sent in the email

--- src/handlers.ts
+++ src/handlers.ts
@@ -9,7 +9,9 @@ export const getSubjectTextAndHtml = (order: { "event-type": OrderEvents, "messa
             html:
                 `
             <div>
-            <h1>Thank you for the order!.Your order will be delivered within 45 mins.Your order id is ${order["message"]._id}</h1>
+            <h2>Thank you for the order!.</h2>
+            <h2>Your order will be delivered within 45 mins.</h2>
+            <h2>Your order id is ${order["message"]._id}</h2>
             <h2>You can see the status of your order here:<a href="${config.get("clientUI.url")}/order/${order["message"]._id}">${order["message"]._id}</a></h2>
             </div>
                 `
@@ -18,11 +20,12 @@ export const getSubjectTextAndHtml = (order: { "event-type": OrderEvents, "messa
 
     else if (order["event-type"] === OrderEvents.ORDER_STATUS_UPDATE) {
         return {
-            subject: "Order Status Update",
+            subject: `Order ${order["message"].orderStatus}`,
             text: `Your order is ${order["message"].orderStatus}`,
             html:
                 `
             <div>
+            <h2>Your order is ${order["message"].orderStatus}</h2>
             <h2>You can see the status of your order here:<a href="${config.get("clientUI.url")}/order/${order["message"]._id}">${order["message"]._id}</a></h2>
             </div>
                 `
